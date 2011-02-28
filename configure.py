b793c083f3892304f5a874b9e0bb5eb8ab5429a6
Jan Chmiel <chmiel.jan@gmail.com>
2011-02-28 15:06:34 +0100

    johnny: first version of configure

--- configure.py
+++ configure.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python
 import logging, os
-import shutil
+import shutil, time
 
 COPYLIST_FILE='copylist.txt'
+homeDirectory = ''
+
+def backupPath(path):
+	try:
+		error = 0
+		d = os.path.join(homeDirectory, '.sweet-home')
+		if not os.path.exists(d):
+			os.mkdir(d)
+		tm = time.localtime()
+		d = os.path.join(homeDirectory, '.sweet-home', "backup-%04d%02d%02d%02d%02d" % (tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min))
+		if not os.path.exists(d):
+			os.mkdir(d)
+		_, tail = os.path.split(path)
+		destPath = os.path.join(d, tail)
+		if os.path.isdir(path):
+			shutil.copytree(path, destPath)
+		elif os.path.isfile(path):
+			shutil.copy(path, destPath)
+	except:
+		return False
+	return True
 
 def copyFiles(copyList, destDir):
-	answerAll = False
 	for e in copyList:
 		dest = os.path.join(destDir, e[1])
 		if os.path.exists(dest):
-			if not answerAll:
-				ans = None
-				while 1:
-					ans = raw_input('%s already exists overwrite (yes/no/all) ? [y/n/a]' % dest)
-					if ans and ans[0] in ('y', 'n', 'a'):
-						break
-					print 'Please respond y, n  or a!'
-				if ans and ans[0] == 'n':
-					continue
-				if ans and ans[0] == 'a':
-					answerAll = True
+			ans = None
+			while 1:
+				ans = raw_input('%s already exists overwrite (yes/no) ? [y/n]' % dest)
+				if ans and ans[0] in ('y', 'n'):
+					break
+				print 'Please respond y, n!'
+			if ans and ans[0] == 'n':
+				continue
+			backupPath(dest)
+			shutil.rmtree(dest)
 
 		print 'copying %s to %s' % (e[0], dest)
 		if os.path.isdir(e[0]):
@@ -31,6 +50,7 @@ def copyFiles(copyList, destDir):
 	pass
 
 def main():
+	global homeDirectory
 	FORMAT = "%(levelname)s: %(message)s"
 	logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 	f = open(COPYLIST_FILE)
@@ -52,9 +72,9 @@ def main():
 		if e not in copyListFiles:
 			logging.warn('%s not mentioned in %s' % (e, COPYLIST_FILE))
 
-	homeDir = os.getenv('HOME')
-	logging.info('HOME is %s' % homeDir)
-	copyFiles(copyList, homeDir)
+	homeDirectory = os.getenv('HOME')
+	logging.info('HOME is %s' % homeDirectory)
+	copyFiles(copyList, homeDirectory)
 
 if __name__ == '__main__':
 	main()
